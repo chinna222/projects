d8b76a064f661efb70b165d072297fad9ab21b50
chinna222 <gondigangadhar@gmail.com>
2024-11-16 17:55:45 +0530

    init commit

---
+++ word-analytics/src/App.jsx
@@ -0,0 +1,15 @@
+import AnalyticsContainer from "./components/AnalyticsContainer";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AnalyticsContainer />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ word-analytics/src/components/BackgroundImage.jsx
@@ -0,0 +1,5 @@
+function BackgroundImage() {
+  return <div className="bg" />;
+}
+
+export default BackgroundImage;
